69462c4c15e7cfde7042a20671d04c8739482d11
Dmytro <166394892+hrunytskyidmytro@users.noreply.github.com>
2024-04-08 15:47:56 +0300

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user')
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const auth = async (req, res, next) => {
     try{
-        const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'kdweueksdsjfij')
-        const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, 'kdweueksdsjfij');
+        const user = await User.findOne({_id: decoded._id, 'tokens.token': token});
         if(!user){
-            throw new Error('Not User')
+            throw new Error('Not User');
         }
-        req.user = user
-        req.token = token
-        next()
+        req.user = user;
+        req.token = token;
+        next();
     }catch(e){
-        res.status(401).send({error: "Please authenticate"})
+        res.status(401).send({error: "Please authenticate"});
     }
 }
 
-module.exports = auth
+module.exports = auth;
