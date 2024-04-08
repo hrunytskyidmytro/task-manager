6be6fd901221c738cc25ec5875176203f71e25b3
Dmytro <166394892+hrunytskyidmytro@users.noreply.github.com>
2024-04-08 15:45:56 +0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-const connectToMongoDB = 'mongodb://localhost:27017/TaskManager'
-const PORT = process.env.PORT || 3000
-const url = `http://localhost:${PORT}`
+const connectToMongoDB = 'mongodb://localhost:27017/TaskManager';
+const PORT = 3000;
+const url = `http://localhost:${PORT}`;
 
 module.exports = {
     connectToMongoDB, PORT, url
